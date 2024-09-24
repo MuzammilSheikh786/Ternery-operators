8c0bb96bcdf5c2d308daf094b643a0a0c69ea057
MuzammilSheikh786 <muzammil26122004@gmail.com>
2024-09-24 12:42:51 +0500

    Ternery operator

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Ternery operators :
+// let person = "Ali";
+// person==="Sahir"?console.log("A good teacher"):console.log("Not a good teacher"); 
+// ternery operators are shortest form of If and else statements:
+// condtion? Message : Message;
+// let empires_call = "Batsmen is not out";
+// empires_call == "out" ? console.log("batsman is out") : console.log("notout");
+// let person2 = "Ahmed";
+// person=="Ahmed"?console.log("A good student"):console.log("bad student");
